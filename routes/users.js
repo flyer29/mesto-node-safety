6b93c7d4a938da8de063ec07d0ebdf051ea5d335
Andrey Borodin <flyer29@bk.ru>
2020-08-20 16:51:09 +0300

    удалил роут создания пользователя

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,6 @@ const {
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/:id', getUserById);
-// usersRouter.post('/', createUser);
 usersRouter.post('/signin', login);
 usersRouter.post('/signup', createUser);
 usersRouter.patch('/me', updateProfile);
