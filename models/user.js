f686c639fd7159bb965ae307d7e49fa16e57a866
Andrey Borodin <flyer29@bk.ru>
2020-08-21 18:28:11 +0300

    убрал password из ответа

--- models/user.js
+++ models/user.js
@@ -36,11 +36,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 8,
     required: true,
+    select: false,
   },
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
