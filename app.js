bd6f79a84e3d4c7a3be89cd860d6c7380aff2cc6
Andrey Borodin <flyer29@bk.ru>
2020-08-24 20:22:16 +0300

    установил helmet в проект

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const cardsRouter = require('./routes/cards.js');
 const usersRouter = require('./routes/users.js');
 const { createUser, login } = require('./controllers/users.js');
@@ -21,6 +22,7 @@ const urlDoesNotExist = (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 };
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
